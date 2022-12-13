381aa022531e754014d7adfb116c9ae798211576
Jadesola Koya <jadeeekoya@gmail.com>
2022-12-12 18:56:16 -0800

    Print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,14 +3,14 @@
 /**
  * main - Prints all the numbers of base 16 in lowercase.
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
 	int num;
 	char letter;
 
-	for (num = 0; num < 110; num++)
+	for (num = 0; num < 10; num++)
 	putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
